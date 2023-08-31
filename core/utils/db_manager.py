e17cc7487e853f93016e2ebf3a3c70dd2da051ac
krys <mentally.gamez.soft@gmail.com>
2023-08-31 18:19:19 +0100

    fix connection to db in container

--- core/utils/db_manager.py
+++ core/utils/db_manager.py
@@ -13,7 +13,8 @@ class DbManager():
     def connect(self):
         login = os.getenv('MONGO_USER')
         password = os.getenv('MONGO_PASSWORD')
-        self.client = MongoClient("mongodb://{}:{}@localhost:27017/".format(login,password))
+        # self.client = MongoClient("mongodb://{}:{}@localhost:27017/".format(login,password))
+        self.client = MongoClient("mongo-db",username=login,password=password,authSource="spam_ham")
 
     def exist_email(self,email) -> bool:
         return self.client.spam_ham.emails.find_one({"email": email})
